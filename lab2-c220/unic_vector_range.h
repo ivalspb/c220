01e738e1f4308a240f2b52bab2dd86d0971f8286
ivalspb <ival.spb@gmail.com>
2024-01-16 18:47:45 +0300

    lab2 sort-- less to range??

--- lab2-c220/unic_vector_range.h
+++ lab2-c220/unic_vector_range.h
@@ -3,25 +3,28 @@
 #include <initializer_list>
 #include <iterator>
 
-template <typename T,  T lower_bound,  T upper_bound>
+template <typename T, typename Compare=std::less>
 class UnicVectorRange_included
 {
+	T lower_bound, upper_bound;
 	std::vector<T> m_v;
 public:
 	UnicVectorRange_included() :m_v(std::vector<T>{}) {};
 	UnicVectorRange_included(const std::initializer_list<T>& il);
 	void push_2unic(const std::initializer_list<T>& il);
 	void delete_2unic(const std::initializer_list<T>& il);
+	void set_range(const T& down, const T& up);
+	void sort(Compare cmp_func);
 };
 
-template<typename T,  T lower_bound,  T upper_bound>
-inline UnicVectorRange_included<T,  lower_bound,  upper_bound>::UnicVectorRange_included(const std::initializer_list<T>& il)
+template<typename T, typename Compare = std::less>
+inline UnicVectorRange_included<T, Compare>::UnicVectorRange_included(const std::initializer_list<T>& il)
 {
 	this->push_2unic(il);
 }
 
-template<typename T,  T lower_bound,  T upper_bound>
-inline void UnicVectorRange_included<T, lower_bound, upper_bound>::push_2unic(const std::initializer_list<T>& il)
+template<typename T, typename Compare = std::less>
+inline void UnicVectorRange_included<T, Compare>::push_2unic(const std::initializer_list<T>& il)
 {
 	for (auto& i : il)
 	{
@@ -40,8 +43,8 @@ inline void UnicVectorRange_included<T, lower_bound, upper_bound>::push_2unic(co
 	}
 }
 
-template<typename T,  T lower_bound,  T upper_bound>
-inline void UnicVectorRange_included<T, lower_bound, upper_bound>::delete_2unic(const std::initializer_list<T>& il)
+template<typename T, typename Compare = std::less>
+inline void UnicVectorRange_included<T, Compare>::delete_2unic(const std::initializer_list<T>& il)
 {
 	for (auto& i : il)
 	{
@@ -55,4 +58,22 @@ inline void UnicVectorRange_included<T, lower_bound, upper_bound>::delete_2unic(
 				}
 		}
 	}
+}
+
+template<typename T, typename Compare = std::less>
+inline void UnicVectorRange_included<T,Compare>::set_range(const T& down, const T& up)
+{
+	if (down > up) 
+	{
+		lower_bound = up;
+		upper_bound = down;
+	}
+	else
+	{
+		lower_bound = down;
+		upper_bound = up;
+	}
+	for (auto& j = m_v.begin(); j != m_v.end(); ++j)
+		if (*j > upper_bound || *j < lower_bound)
+			j = m_v.erase(j);
 }
