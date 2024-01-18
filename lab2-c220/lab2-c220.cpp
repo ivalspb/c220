adf00e5c3ccd2a97fd9c7c52dd75301765c9296d
ALEIVAN@live.spbstu.ru <ALEIVAN@live.spbstu.ru>
2024-01-18 11:22:49 +0300

    lab2.1.1 --compare--

--- lab2-c220/lab2-c220.cpp
+++ lab2-c220/lab2-c220.cpp
@@ -1,7 +1,27 @@
 #include "unic_vector_range_included.h"
+#include "unic_vector_range_inherit.h"
+#include <iostream>
+#include <iterator>
 
 int main(int argc, char** argv)
 {
+	std::cout << "init unic vector<int> at [0,3]:\n";
+	UnicVectorRange_included<int> vi1({ 3,2,4,-3,1,3 }, 0, 3);
+	std::copy(vi1.begin(), vi1.end(), std::ostream_iterator<int>(std::cout, ", "));
+	vi1.sort();
+	vi1.set_range(-3, +3);
+	vi1.push_2unic({ 0,-2,4,-1,2 });
+	std::cout << "\nsort + set_range [-3,3] + push vector:\n";
+	std::copy(vi1.begin(), vi1.end(), std::ostream_iterator<int>(std::cout, ", "));
+
+	std::cout << "\n--------------------------------\n";
+	std::cout << "init unic vector <const char*> at [ab,bc]\n";
+	UnicVectorRange_included < const char*, charcmp> vc1({ "aaa","abc","def","back","bcd","aba" }, "ab", "bc");
+	std::copy(vc1.begin(), vc1.end(), std::ostream_iterator<int>(std::cout, ", "));
+	vc1.sort();
+	vc1.set_range("aa", "ba");
+	std::cout << "\nsort + set_range [aa,ba]:\n";
+	std::copy(vi1.begin(), vi1.end(), std::ostream_iterator<int>(std::cout, ", "));
 
 	return 0;
 }
--- lab2-c220/unic_vector_range_included.h
+++ lab2-c220/unic_vector_range_included.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <initializer_list>
 #include <iterator>
+#include <algorithm>
 
 template <typename T, typename Compare=std::less<T> >
 class UnicVectorRange_included
@@ -9,29 +10,30 @@ class UnicVectorRange_included
 	T lower_bound, upper_bound;
 	std::vector<T> m_v;
 public:
-	UnicVectorRange_included() :m_v(std::vector<T>{}) {};
-	UnicVectorRange_included(const std::initializer_list<T>& il, const Compare &cmp_func);
+	UnicVectorRange_included()=default;
+	UnicVectorRange_included(const std::initializer_list<T>& il, const T&lo, const T& hi,  Compare cmp_func=std::less<T>());
 	UnicVectorRange_included(const typename UnicVectorRange_included& other)=default;
 	UnicVectorRange_included(typename UnicVectorRange_included&& tmp_other) = default;
 	UnicVectorRange_included& operator=(const typename UnicVectorRange_included& other) = default;
 	UnicVectorRange_included& operator=(typename UnicVectorRange_included&& tmp_other) = default;
-	void push_2unic(const std::initializer_list<T>& il, const Compare& cmp_func);
-	void delete_2unic(const std::initializer_list<T>& il, const Compare& cmp_func);
-	void set_range(const T& down, const T& up, const Compare& cmp_func);
-	void sort(const Compare &cmp_func);
-	typedef T* iterator;
-	iterator begin() { return m_v.begin() };
-	iterator end() { return m_v.end() };
+	void push_2unic(const std::initializer_list<T>& il,  Compare cmp_func = std::less<T>());
+	void pop_2unic(const std::initializer_list<T>& il,  Compare cmp_func = std::less<T>());
+	void set_range(const T& down, const T& up,  Compare cmp_func = std::less<T>());
+	void sort(Compare cmp_func = std::less<T>());
+	auto begin() { return m_v.begin(); }
+	auto end() { return m_v.end(); }
 };
 
 template<typename T, typename Compare>
-inline UnicVectorRange_included<T, Compare>::UnicVectorRange_included(const std::initializer_list<T>& il, const Compare& cmp_func)
+inline UnicVectorRange_included<T, Compare>::UnicVectorRange_included(const std::initializer_list<T>& il, const T& lo, const T& hi, Compare cmp_func)
 {
+	lower_bound = lo;
+	upper_bound = hi;
 	this->push_2unic(il,cmp_func);
 }
 
 template<typename T, typename Compare>
-inline void UnicVectorRange_included<T, Compare>::push_2unic(const std::initializer_list<T>& il, const Compare& cmp_func)
+inline void UnicVectorRange_included<T, Compare>::push_2unic(const std::initializer_list<T>& il,  Compare cmp_func)
 {
 	for (auto& i : il)
 		if (!cmp_func(i, lower_bound) && !cmp_func(upper_bound, i) && (std::find(m_v.begin(), m_v.end(), i) == m_v.end()))
@@ -39,7 +41,7 @@ inline void UnicVectorRange_included<T, Compare>::push_2unic(const std::initiali
 }
 
 template<typename T, typename Compare>
-inline void UnicVectorRange_included<T, Compare>::delete_2unic(const std::initializer_list<T>& il, const Compare& cmp_func)
+inline void UnicVectorRange_included<T, Compare>::pop_2unic(const std::initializer_list<T>& il,  Compare cmp_func)
 {
 	for (auto& i : il)
 	{
@@ -56,7 +58,7 @@ inline void UnicVectorRange_included<T, Compare>::delete_2unic(const std::initia
 }
 
 template<typename T, typename Compare>
-inline void UnicVectorRange_included<T,Compare>::set_range(const T& down, const T& up, const Compare& cmp_func)
+inline void UnicVectorRange_included<T,Compare>::set_range(const T& down, const T& up, Compare cmp_func)
 {
 	if (down > up) 
 	{
@@ -68,14 +70,17 @@ inline void UnicVectorRange_included<T,Compare>::set_range(const T& down, const 
 		lower_bound = down;
 		upper_bound = up;
 	}
-	for (auto& j = m_v.begin(); j != m_v.end(); ++j)
+	for (auto j = m_v.begin(); j != m_v.end(); ++j)
 		if (cmp_func(upper_bound, *j)  || cmp_func(*j, lower_bound))
 			j = m_v.erase(j);
 }
 
 template<typename T, typename Compare>
-inline void UnicVectorRange_included<T, Compare>::sort(const Compare &cmp_func)
+inline void UnicVectorRange_included<T, Compare>::sort(Compare cmp_func)
 {
 	std::sort(m_v.begin(), m_v.end(), cmp_func);
 }
 
+struct charcmp {
+	bool operator()(const char* s1, const char* s2) const { return strcmp(s1, s2) < 0; }
+};
