fda5ff0ee1accadc4dedb7105b8779247d8b0ab8
ivalspb <ival.spb@gmail.com>
2024-02-13 18:47:34 +0300

    lab5.5--

--- lab5-c220/Human.cpp
+++ lab5-c220/Human.cpp
@@ -1,7 +1,8 @@
 #include "Human.h"
 
-//void Human::child(std::shared_ptr<Human> mother, std::shared_ptr<Human> father, std::string& name)
-//{
-	//m_name = name;
-
-//}
+void Human::child(std::shared_ptr<Human> mother, std::shared_ptr<Human> father, std::string& name)
+{
+	std::vector<std::weak_ptr<Human>> childfree{};
+	Human newborn (name, true, mother, father, childfree);
+	mother->m_childs.push_back(std::make_shared<Human>(newborn));
+}
